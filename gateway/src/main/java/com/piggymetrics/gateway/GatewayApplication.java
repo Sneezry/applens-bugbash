66a77d5eecc1510e88d729d95c047f9fa60ee327
Zhe Li <zlhe@microsoft.com>
2020-04-23 18:20:33 +0800

    init

---
+++ gateway/src/main/java/com/piggymetrics/gateway/GatewayApplication.java
@@ -0,0 +1,25 @@
+package com.piggymetrics.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableZuulProxy
+public class GatewayApplication {
+    @Override
+    protected void finalize() throws Throwable {
+        while (true) {
+            Thread.yield();
+        }
+    }
+
+	public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            GatewayApplication f = new GatewayApplication();
+        }
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+}
